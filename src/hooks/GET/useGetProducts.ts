489da1770dff93837fb59d68c97fba40729153ae
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-07-21 21:15:36 -0300

    refactor: adjusting query to get all products with filters and total products

--- src/hooks/GET/useGetProducts.ts
+++ src/hooks/GET/useGetProducts.ts
@@ -1,32 +1,62 @@
 import { API } from "@/services/api";
+import { GetAllProducts, GetProductsParams } from "@/types/products";
 import { useQuery } from "@tanstack/react-query";
 
-type GetProductsParams = {
-  page: number;
-  perPage: number;
-};
+export const getProducts = async ({
+  page,
+  perPage,
+  filter,
+}: GetProductsParams) => {
+  const getParams = () => {
+    let query = `page: ${page}, perPage: ${perPage},`;
+    let keys: any[] = [];
+    for (let item of Object.keys(filter)) {
+      if (filter[item as keyof typeof filter]) keys.push(item);
+    }
 
-export const getProducts = async ({ page, perPage }: GetProductsParams) => {
-  const response = await API.post("/", {
+    if (!keys.length) return query;
+
+    const filters = keys.reduce((acc: string, cur: any) => {
+      acc += `${cur === "name" ? `q` : cur}: "${
+        filter[cur as keyof typeof filter]
+      }", `;
+      return acc;
+    }, "");
+
+    return query + `filter: {${filters}}`;
+  };
+
+  const query = getParams();
+
+  const { data } = await API.post("/", {
     query: `
       query {
-        allProducts(page: ${page}, perPage: ${perPage}) {
-          id,
-          name,
-          image_url,
-          price_in_cents
+        allProducts(${query}) {
+          id
+          name
+          image_url
+          price_in_cents,
+          category,
+        },
+        _allProductsMeta ${query ? `(${query})` : null} {
+          count
         }
       }
       `,
   });
 
-  return response.data;
+  const allProducts = data && {
+    allProducts: data.data.allProducts,
+    total: data.data._allProductsMeta.count,
+  };
+
+  return allProducts;
 };
 
-export const useGetProducts = ({ page, perPage }: GetProductsParams) => {
-  const { data, isLoading } = useQuery({
-    queryKey: ["GET_products", page],
-    queryFn: () => getProducts({ page, perPage }),
+export const useGetProducts = (params: GetProductsParams) => {
+  const { data, isLoading } = useQuery<GetAllProducts>({
+    queryKey: ["GET_products", params],
+    queryFn: () => getProducts(params),
   });
 
   return {
