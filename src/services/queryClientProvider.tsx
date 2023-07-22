89614f7bb5708d65a1dfb5705838786e49e9140f
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-07-21 21:37:31 -0300

    refactor: import react query tolls

--- src/services/queryClientProvider.tsx
+++ src/services/queryClientProvider.tsx
@@ -4,6 +4,7 @@ import {
   QueryClient,
   QueryClientProvider as Provider,
 } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode } from "react";
 
 type QueryClientProps = {
@@ -13,5 +14,10 @@ type QueryClientProps = {
 const queryClient = new QueryClient();
 
 export const QueryClientProvider = ({ children }: QueryClientProps) => {
-  return <Provider client={queryClient}>{children}</Provider>;
+  return (
+    <Provider client={queryClient}>
+      {children}
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </Provider>
+  );
 };
