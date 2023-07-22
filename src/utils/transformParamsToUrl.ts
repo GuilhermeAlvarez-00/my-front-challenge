a922125824a968ab7828f1f7b9c8100d4206bb13
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-07-22 15:36:27 -0300

    refactor: adjusting query filters and params

--- src/utils/transformParamsToUrl.ts
+++ src/utils/transformParamsToUrl.ts
@@ -1,24 +1,59 @@
 import { GetProductsParams } from "@/types/products";
 
-export const transformParamsToUrl = ({
-  page,
-  perPage,
-  filter,
-}: GetProductsParams) => {
-  let query = `page: ${page}, perPage: ${perPage},`;
-  let keys: any[] = [];
-  for (let item of Object.keys(filter)) {
-    if (filter[item as keyof typeof filter]) keys.push(item);
-  }
+type ParamKey<T> = keyof T;
 
-  if (!keys.length) return query;
+export const transformParamsToUrl = (params: GetProductsParams) => {
+  const paramsQuery = Object.keys(params).filter((item) => item !== "filter");
+  const metaQuery = Object.keys(params).filter(
+    (item) => item !== "sortField" && item !== "sortOrder" && item !== "filter"
+  );
+  const paramsFilters = params.filter;
 
-  const filters = keys.reduce((acc: string, cur: any) => {
-    acc += `${cur === "name" ? `q` : cur}: "${
-      filter[cur as keyof typeof filter]
-    }", `;
+  const query = paramsQuery.reduce((acc, cur) => {
+    acc += `${cur}: ${
+      typeof params[cur as keyof typeof params] === "string" &&
+      !params[cur as keyof typeof params]
+        ? `"${params[cur as keyof typeof params]}"`
+        : typeof params[cur as keyof typeof params] === "string"
+        ? `"${params[cur as keyof typeof params]}"`
+        : params[cur as keyof typeof params]
+    }, `;
+
     return acc;
   }, "");
 
-  return query + `filter: {${filters}}`;
+  const meta = metaQuery.reduce((acc, cur) => {
+    acc += `${cur}: ${
+      typeof params[cur as keyof typeof params] === "string" &&
+      !params[cur as keyof typeof params]
+        ? `"${params[cur as keyof typeof params]}"`
+        : typeof params[cur as keyof typeof params] === "string"
+        ? `"${params[cur as keyof typeof params]}"`
+        : params[cur as keyof typeof params]
+    }, `;
+
+    return acc;
+  }, "");
+
+  const filters = Object.keys(paramsFilters).reduce((acc, cur) => {
+    if (
+      cur === "category" &&
+      !params.filter[cur as ParamKey<typeof params.filter>]
+    )
+      return acc;
+
+    acc += ` ${cur}: ${`"${
+      params.filter[cur as ParamKey<typeof params.filter>]
+    }"`}, `;
+
+    return acc;
+  }, "");
+
+  const finalQuery = query + `filter: { ${filters} }`;
+  const finalMeta = meta + `filter: { ${filters} }`;
+
+  return {
+    query: finalQuery,
+    meta: finalMeta,
+  };
 };
