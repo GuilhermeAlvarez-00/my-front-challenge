cad88a0ed198e5aa0052b40b12d633edd60584fb
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-07-21 21:35:55 -0300

    refactor: import utils

--- src/hooks/GET/useGetProducts.ts
+++ src/hooks/GET/useGetProducts.ts
@@ -1,5 +1,6 @@
 import { API } from "@/services/api";
 import { GetAllProducts, GetProductsParams } from "@/types/products";
+import { transformParamsToUrl } from "@/utils/transformParamsToUrl";
 import { useQuery } from "@tanstack/react-query";
 
 export const getProducts = async ({
@@ -7,26 +8,7 @@ export const getProducts = async ({
   perPage,
   filter,
 }: GetProductsParams) => {
-  const getParams = () => {
-    let query = `page: ${page}, perPage: ${perPage},`;
-    let keys: any[] = [];
-    for (let item of Object.keys(filter)) {
-      if (filter[item as keyof typeof filter]) keys.push(item);
-    }
-
-    if (!keys.length) return query;
-
-    const filters = keys.reduce((acc: string, cur: any) => {
-      acc += `${cur === "name" ? `q` : cur}: "${
-        filter[cur as keyof typeof filter]
-      }", `;
-      return acc;
-    }, "");
-
-    return query + `filter: {${filters}}`;
-  };
-
-  const query = getParams();
+  const query = transformParamsToUrl({ page, perPage, filter });
 
   const { data } = await API.post("/", {
     query: `
